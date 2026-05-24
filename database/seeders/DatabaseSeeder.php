f3eabe6fdffd2afa3949e58ab170d313629c1406
Prince Anand <pk933466@gmail.com>
2026-05-25 01:50:13 +0530

    Remove factory from seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,14 +14,14 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // User::factory(10)->create();
+{
+    User::create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => bcrypt('password'),
+        'role' => 'student',
+    ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
-        $this->call(TutzySeeder::class);
-    }
+    $this->call(TutzySeeder::class);
+}
 }
